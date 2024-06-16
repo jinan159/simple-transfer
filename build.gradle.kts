e80db5050ad451c6b4c00fe4a2a3377936907c3c
jinan159 <wlsdhks0423@naver.com>
2024-06-17 01:10:19 +0900

    init

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.2.6"
+    id("io.spring.dependency-management") version "1.1.5"
+}
+
+group = "com.kakaobank.simpletransfer"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // springboot
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+
+    // flyway
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.flywaydb:flyway-database-oracle")
+
+    // kafka
+    implementation("org.springframework.kafka:spring-kafka")
+
+    // oracle
+    runtimeOnly("com.oracle.database.jdbc:ojdbc11")
+
+    // test
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.kafka:spring-kafka-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/main/java/com/kakaobank/simpletransfer/KakaobankSimpleTransferApplication.java
@@ -0,0 +1,11 @@
+package com.kakaobank.simpletransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaobankSimpleTransferApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KakaobankSimpleTransferApplication.class, args);
+    }
+}
